14453a40dfe0a0f724d7078020c8ebe8f1057c40
Andres Delgado <andresdelgado335@gmail.com>
2023-01-17 13:38:53 -0600

    finished exercise

---
+++ src/main/java/com/codeup/blogapp/PostController.java
@@ -0,0 +1,29 @@
+package com.codeup.blogapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PostController {
+    @GetMapping("/posts")
+    @ResponseBody
+    public String posts() {
+        return "New posts!";
+    }
+
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String id(@PathVariable String id) {
+        return "Hello " + id + "!";
+    }
+
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String createPost() {
+        return "New Post";
+    }
+}
