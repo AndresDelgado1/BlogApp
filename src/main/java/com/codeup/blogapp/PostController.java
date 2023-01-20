90173e42aac0ff0761bcd88f7ac515bc7195c14b
Andres Delgado <andresdelgado335@gmail.com>
2023-01-20 10:29:53 -0600

    finished repositories exercises

--- src/main/java/com/codeup/blogapp/PostController.java
+++ src/main/java/com/codeup/blogapp/PostController.java
@@ -1,29 +1,53 @@
 package com.codeup.blogapp;
 
+import com.codeup.blogapp.model.Post;
+import com.codeup.blogapp.repositories.PostRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
-    @GetMapping("/posts")
-    @ResponseBody
-    public String posts() {
-        return "New posts!";
+
+    private final PostRepository postDao;
+
+    public PostController(PostRepository postDao) {
+        this.postDao = postDao;
     }
 
 
+
+    @GetMapping("/posts")
+    public String allPosts(Model model) {
+        model.addAttribute("posts", postDao.findAll());
+        return "/posts/index";
+    }
+
+
+
+
     @GetMapping("/posts/{id}")
-    @ResponseBody
     public String id(@PathVariable String id) {
         return "Hello " + id + "!";
     }
 
 
+//    @GetMapping("/posts/create")
+//    public String getCreate(Model model) {
+//        model.addAttribute("post", new Post());
+//        return "/posts/create";
+//    }
     @GetMapping("/posts/create")
-    @ResponseBody
-    public String createPost() {
-        return "New Post";
+        public String getCreate() {
+        return "/posts/create";
+    }
+
+    @PostMapping("/posts/create")
+    public String createPost(@RequestParam(name="title") String title, @RequestParam(name="body") String body ){
+        postDao.save(new Post(title, body));
+        return "redirect:/posts";
     }
 }
--- src/main/java/com/codeup/blogapp/repositories/PostRepository.java
+++ src/main/java/com/codeup/blogapp/repositories/PostRepository.java
@@ -1,8 +1,8 @@
-//package com.codeup.blogapp.repositories;
-//
-//import com.codeup.blogapp.model.Post;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface PostRepository extends JpaRepository<Post, Long> {
-//
-//}
+package com.codeup.blogapp.repositories;
+
+import com.codeup.blogapp.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
